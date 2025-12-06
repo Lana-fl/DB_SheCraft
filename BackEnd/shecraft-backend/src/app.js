52907d00ab244f6477de770f62d75c0619f297a0
RDGH5825825 <121638780+RDGH5825825@users.noreply.github.com>
2025-12-07 00:04:50 +0200

    backend pics

--- BackEnd/shecraft-backend/src/app.js
+++ BackEnd/shecraft-backend/src/app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const path = require("path");
+
 const customerRoutes = require("./routes/customerRoute");
 const { requestLogger } = require("./middleware"); // optional but nice
 
@@ -12,6 +14,8 @@ const authRoutes = require("./routes/authRoutes");
 const orderRoute = require("./routes/OrderRoute");   // <-- ADDED
 
 const app = express();
+
+app.use("/images", express.static(path.join(__dirname, "../public/images")));
 
 // Global middleware
 app.use(cors());
